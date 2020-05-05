3250f9be4cb43565754a305054d162d756472b09
Fernando Zunino <fernando@flowics.com>
2020-05-05 01:21:55 -0300

    Fix device-width and tidy sources
    Have to use <meta name="viewport" content="width=device-width,
    initial-scale=1.0"> in order for ui variation to work properly

--- bitmovin/src/index.ts
+++ bitmovin/src/index.ts
@@ -22,23 +22,36 @@ document.addEventListener("DOMContentLoaded", function (event) {
         "https://viz.flowics.com/public/7f1abbadc05d2db270a52cad6360327b/5ea703b94fa8ca5176941496/live",
     },
   };
-  var source = {
-    hls:
-      "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-    // "https://cdn3.wowza.com/1/bGxnZ2dzbjk3TEJr/SVd1TlZu/hls/live/playlist.m3u8",
-    dash: "http://demo-dash-live.zahs.tv/hd/manifest.mpd?timeshift=100",
-
-    poster:
-      "https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg",
+  var sources = {
+    vod: {
+      parkour: {
+        hls:
+          "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
+        poster:
+          "https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg",
+      },
+    },
+    live: {
+      wowza: {
+        hls:
+          "https://cdn3.wowza.com/1/bGxnZ2dzbjk3TEJr/SVd1TlZu/hls/live/playlist.m3u8",
+        poster:
+          "https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg",
+      },
+      cnn: {
+        dash: "http://demo-dash-live.zahs.tv/hd/manifest.mpd?timeshift=100",
+        poster:
+          "https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg",
+      },
+    },
   };
 
   var container = document.getElementById("bitmovin-player");
   if (container) {
     const player = new Player(container, config);
     const uiInstance = buildFlowicsUI(player, uiConfig);
-    // const uiInstance = UIFactory.buildDefaultUI(player, {});
 
-    player.load(source).then(
+    player.load(sources.live.cnn).then(
       function () {
         console.log("Successfully created Bitmovin Player instance");
       },
